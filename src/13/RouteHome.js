405e2a1a52964a0c4702c2cfe9d881559aa6a876
happy950430 <happy950430@gmail.com>
2024-10-11 17:50:05 +0900

    App.js'sRoute

--- src/13/RouteHome.js
+++ src/13/RouteHome.js
@@ -11,14 +11,17 @@ export default function RouteHome() {
     <h1 className="text-3xl justify-center items-center 
     mb-5 p-5 bg-slate-300">page1</h1>
     <ul>
-     <li><Link to='/p1/사과'>사과</Link></li>
-    </ul>
+    <li><Link to='/p1/🍎/사과'>사과 🍎</Link></li>
+    <li><Link to='/p1/🍌/바나나'>바나나 🍌</Link></li>
+    <li><Link to='/p1/🥕/당근'>당근 🥕</Link></li>    </ul>
    </div>
    <div>
     <h1 className="text-3xl justify-center items-center
     mb-5 p-5 bg-slate-300">page2</h1>
     <ul>
-    <li><Link to='/p2?item=바나나&item2=사과'>바나나</Link></li>
+    <li><Link to='/p2?item=🍎&item2=사과'>사과 🍎</Link></li>
+    <li><Link to='/p2?item=🍌&item2=바나나'>바나나 🍌</Link></li>
+    <li><Link to='/p2?item=🥕&item2=당근'>당근 🥕</Link></li>
    </ul>
    </div>
    </div>
--- src/App.js
+++ src/App.js
@@ -3,45 +3,60 @@ import './App.css';
 // import MyClock from './02/MyClock';
 // import Hello from './01/Hello';
 import { IoMdHome } from "react-icons/io";
-//  import BoxOffice from './07/BoxOffice';
+// import BoxOffice from './07/BoxOffice';
 // import MyDiv1 from './03/MyDiv1';
 // import MyList from './04/MyList';
 // import Lotto from './05/Lotto';
 // import FoodMain from './06/FoodMain';
 // import MyBox from './08/MyBox';
-//import Traffic from './09/Traffic';
+// import Traffic from './09/Traffic';
 // import MyRef from './10/MyRef';
 // import Gallery from './11/Gallery';
 // import Festival from './12/Festival';
-import RouteMain from './13/RouteMain';
+// import RouteMain from './13/RouteMain';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="w-full xl:w-10/12 h-screen mx-auto
                     flex flex-col justify-center items-center">
       <header className='w-full h-20
                         flex justify-between items-center
                        bg-slate-500'>
         <p className='text-2xl font-bold p-5'>k-digital 8기 React</p>
+        <ul className='flex justify-center items-center'>
+              <li className='mx-4 p-2 hoover: bg-slate-400 hoover:text-white rounded-md'>시계</li>
+              <li className='mx-4 p-2 hoover: bg-slate-400 hoover:text-white rounded-md'>로또생성기</li>
+              <li className='mx-4 p-2 hoover: bg-slate-400 hoover:text-white rounded-md'>푸드뱅크</li>
+              <li className='mx-4 p-2 hoover: bg-slate-400 hoover:text-white rounded-md'>박스오피스</li>
+              <li className='mx-4 p-2 hoover: bg-slate-400 hoover:text-white rounded-md'>교통사고</li>
+              <li className='mx-4 p-2 hoover: bg-slate-400 hoover:text-white rounded-md'>관광</li>
+              <li className='mx-4 p-2 hoover: bg-slate-400 hoover:text-white rounded-md'>축제</li>
+        </ul>
         <p className='text-3xl font-bold p-5'><IoMdHome />
         </p>
       </header>
-
+      
       <main className='w-full grow
                       flex flex-col -*items-center
                       overflow-y-auto'>
-
-        {/* {<MyClock />} */}
+        <Routes>
+        <Route path = '/' element = {<MyClock />} />
+        <Lotto path = '/' element = {<Lotto />} /> 
+        <FoodMain path = '/' element = {<FoodMain />} />
+        <BoxOffice path = '/' element = {<BoxOffice />} /> 
+        <MyBox path = '/' element = {<MyBox />} /> 
+        <Traffic path = '/' element = {<Traffic />} /> 
+        <Gallery path = '/' element = {<Gallery />} /> 
+        <Festival path = '/' element = {<Festival />} /> 
+        </Routes>
+        
         {/* <MyDiv1 /> */}
         {/* <MyList /> */}
-        {/* <Lotto /> */}
-        {/* {<FoodMain />} */}
-        {/* <BoxOffice /> */}
-        {/* <MyBox /> */}
-        {/*<Traffic /> */}
+        {/* <RouteMain /> */}
         {/* <MyRef /> */}
-{/* <Gallery /> */}
-{/* <Festival /> */}
-<RouteMain />
       </main>
       <footer className='w-full h-20 flex-shrink-0
                         flex justify-center items-center
@@ -51,6 +66,7 @@ function App() {
       </footer>
 
     </div>
+    </BrowserRouter>
   );
 }
 
